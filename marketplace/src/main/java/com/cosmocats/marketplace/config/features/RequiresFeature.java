3b5e469bec84af885a30d084382dc62e14d9e162
lady Maria <159808384+maari88@users.noreply.github.com>
2025-11-10 10:00:17 +0200

    Create RequiresFeature.java

---
+++ marketplace/src/main/java/com/cosmocats/marketplace/config/features/RequiresFeature.java
@@ -0,0 +1,12 @@
+package com.cosmocats.marketplace.config.features;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiresFeature {
+    Feature value();
+}
