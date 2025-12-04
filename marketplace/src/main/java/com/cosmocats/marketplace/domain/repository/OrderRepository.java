b7950a1528e63fb7d89a6bab12c4c1beb99c23c1
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-04 20:11:28 +0200

    Update OrderRepository.java

--- marketplace/src/main/java/com/cosmocats/marketplace/domain/repository/OrderRepository.java
+++ marketplace/src/main/java/com/cosmocats/marketplace/domain/repository/OrderRepository.java
@@ -4,8 +4,10 @@ import com.cosmocats.marketplace.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByCustomerId(String customerId);
+    Optional<Order> findByOrderNumber(String orderNumber);
 }
 
