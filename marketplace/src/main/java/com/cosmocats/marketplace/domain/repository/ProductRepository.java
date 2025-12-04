40368f28f4ae4313be328be00762e2e201dfcb8e
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-04 20:11:26 +0200

    Update ProductRepository.java

--- marketplace/src/main/java/com/cosmocats/marketplace/domain/repository/ProductRepository.java
+++ marketplace/src/main/java/com/cosmocats/marketplace/domain/repository/ProductRepository.java
@@ -1,15 +1,24 @@
 package com.cosmocats.marketplace.domain.repository;
 
 import com.cosmocats.marketplace.domain.Product;
+import com.cosmocats.marketplace.domain.repository.projection.TopProductProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findBySku(String sku);
 
     Page<Product> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String description, Pageable pageable);
+    @Query("SELECT p.name AS name, SUM(oi.quantity) AS totalSold " +
+            "FROM OrderItem oi " +
+            "JOIN oi.product p " +
+            "GROUP BY p.id, p.name " +
+            "ORDER BY totalSold DESC")
+    List<TopProductProjection> findTopSellingProducts();
 }
 
