8e3d07ca00cc7ead7dc7447c8844eb7b870bde37
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-08 10:58:31 +0200

    Update AbstractIntegrationTest.java

--- marketplace/src/test/java/com/cosmocats/marketplace/AbstractIntegrationTest.java
+++ marketplace/src/test/java/com/cosmocats/marketplace/AbstractIntegrationTest.java
@@ -4,18 +4,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
 public abstract class AbstractIntegrationTest {
+    
+    static final PostgreSQLContainer<?> postgres;
 
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
-            .withDatabaseName("cosmocats")
-            .withUsername("test")
-            .withPassword("test");
+    static {
+        postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+                .withDatabaseName("cosmocats")
+                .withUsername("test")
+                .withPassword("test");
+
+        postgres.start();
+    }
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
@@ -25,11 +29,8 @@ public abstract class AbstractIntegrationTest {
 
         registry.add("spring.datasource.driverClassName", () -> "org.postgresql.Driver");
         registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
-
         registry.add("spring.jpa.database", () -> "POSTGRESQL");
-
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
-
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
         registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
     }
 }
