b4e7fe78c11326f5efeb762db947d56385e740fd
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-08 10:24:13 +0200

    Create OrderItemRepository.java

---
+++ marketplace/src/main/java/com/cosmocats/marketplace/domain/repository/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.cosmocats.marketplace.domain.repository;
+import com.cosmocats.marketplace.domain.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {}
