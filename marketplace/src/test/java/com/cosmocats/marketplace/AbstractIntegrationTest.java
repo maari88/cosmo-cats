9cca3126e7fc8732f325435e25b148204cdc8519
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-08 11:07:48 +0200

    Update AbstractIntegrationTest.java

--- marketplace/src/test/java/com/cosmocats/marketplace/AbstractIntegrationTest.java
+++ marketplace/src/test/java/com/cosmocats/marketplace/AbstractIntegrationTest.java
@@ -1,39 +1,30 @@
 package com.cosmocats.marketplace;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection; 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
 public abstract class AbstractIntegrationTest {
-    
-    static final PostgreSQLContainer<?> postgres;
-
-    static {
-        postgres = new PostgreSQLContainer<>("postgres:16-alpine")
-                .withDatabaseName("cosmocats")
-                .withUsername("test")
-                .withPassword("test");
-        postgres.start();
-    }
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withDatabaseName("cosmocats")
+            .withUsername("test")
+            .withPassword("test");
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-
-        registry.add("spring.datasource.driverClassName", () -> "org.postgresql.Driver");
-        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
-
-        registry.add("spring.jpa.database", () -> "POSTGRESQL");
-        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
 
         registry.add("spring.liquibase.enabled", () -> "false");
 
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+
+        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
     }
 }
