b7a57f6b378923987733d3d35e5dca401f7e6ae9
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-08 10:37:50 +0200

    Update AbstractIntegrationTest.java

--- marketplace/src/test/java/com/cosmocats/marketplace/AbstractIntegrationTest.java
+++ marketplace/src/test/java/com/cosmocats/marketplace/AbstractIntegrationTest.java
@@ -22,7 +22,9 @@ public abstract class AbstractIntegrationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
-
+        
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+
+        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
     }
 }
