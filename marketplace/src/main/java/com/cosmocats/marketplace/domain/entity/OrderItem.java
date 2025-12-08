b5db29b87f3afc451240d6f7eb56a559b6c3b8b6
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-08 11:36:40 +0200

    Create OrderItem.java

---
+++ marketplace/src/main/java/com/cosmocats/marketplace/domain/entity/OrderItem.java
@@ -0,0 +1,31 @@
+package com.cosmocats.marketplace.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private String productName;
+    private Integer quantity;
+    private Double pricePerUnit;
+    private Double lineTotal;
+}
+
