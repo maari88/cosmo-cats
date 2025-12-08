15fddae2312d3e224717945246c62c510e57668e
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-08 11:36:23 +0200

    Update OrderItemRepository.java

--- marketplace/src/main/java/com/cosmocats/marketplace/domain/repository/OrderItemRepository.java
+++ marketplace/src/main/java/com/cosmocats/marketplace/domain/repository/OrderItemRepository.java
@@ -1,5 +1,5 @@
 package com.cosmocats.marketplace.domain.repository;
-import com.cosmocats.marketplace.domain.OrderItem;
+import com.cosmocats.marketplace.domain.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
