38da8feb16ad0bd6acb551dcec69574a39378c68
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-08 10:24:11 +0200

    Update OrderItem.java

--- marketplace/src/main/java/com/cosmocats/marketplace/domain/OrderItem.java
+++ marketplace/src/main/java/com/cosmocats/marketplace/domain/OrderItem.java
@@ -16,6 +16,10 @@ public class OrderItem {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
