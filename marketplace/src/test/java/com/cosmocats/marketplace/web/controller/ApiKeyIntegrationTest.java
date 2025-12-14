4f1deeaad26d1114c274cffcaa015fb6fddd629c
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-14 19:00:48 +0200

    Update ApiKeyIntegrationTest.java

--- marketplace/src/test/java/com/cosmocats/marketplace/web/controller/ApiKeyIntegrationTest.java
+++ marketplace/src/test/java/com/cosmocats/marketplace/web/controller/ApiKeyIntegrationTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest 
+@SpringBootTest
 @AutoConfigureMockMvc
 public class ApiKeyIntegrationTest {
 
