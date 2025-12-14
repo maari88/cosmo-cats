5604a647ddd858eb4397efa0b7a0edfa2c94f85f
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-14 16:55:40 +0200

    Create NoAuthSecurityConfig.java

---
+++ marketplace/src/main/java/com/cosmocats/marketplace/config/NoAuthSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.cosmocats.marketplace.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Profile("no-auth")
+public class NoAuthSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain noAuthFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+
+        return http.build();
+    }
+}
